3ae6f2eec1998cabda7f244a017e5549b3c08a65
Tiffany Mackay <88056784+tiffanymackay@users.noreply.github.com>
2024-11-11 21:33:01 -0800

    fix bug

--- ai-weather-app/src/Weather.js
+++ ai-weather-app/src/Weather.js
@@ -27,7 +27,7 @@ const Weather = ({ defaultCity }) => {
   };
 
   const search = useCallback(() => {
-    const apiKey = 'YOUR_API_KEY';
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
     axios.get(url).then(showTemperature);
   }, [city]);
