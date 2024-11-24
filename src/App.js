affddc5f1b0ef4123111af7e3ed5a10aaa0c4fea
elykh1993 <120404324+elykh1993@users.noreply.github.com>
2024-11-24 13:47:10 +0330

    App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <ErrorBoundary>
-        <Weather defaultCity="Seattle" />
+        <Weather defaultCity="Tehran" />
       </ErrorBoundary>
     </div>
   );
